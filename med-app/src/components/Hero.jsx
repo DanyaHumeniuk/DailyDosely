0c0a17cde82b808936b98a4237e8ecc123238809
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-06-01 23:36:04 -0700

    Made componenets responsive

--- med-app/src/components/Hero.jsx
+++ med-app/src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
     <div className="h-screen w-screen flex flex-col bg-[#e0e5ec] justify-center items-center gap-4 px-4">
       <div className="flex flex-col gap-6 w-full h-full items-center justify-center mx-auto">
 
-        <div className="flex flex-col w-full max-w-2xl aspect-video sm:max-w-lg lg:max-w-4xl bg-[#e0e5ec] rounded-[50px] shadow-neu gap-2 justify-center items-center p-4">
-          <div className="flex w-full bg-[#e0e5ec] rounded-[50px] shadow-neu h-full"></div>
-          <div className="flex flex-row w-full rounded-[50px] h-full gap-2">
-            <div className="flex rounded-full bg-[#e0e5ec] w-1/2 h-full border border-black"></div>
-            <div className="flex bg-[#e0e5ec] border border-black rounded-[50px] w-full h-full"></div>
+        <div className="flex flex-col w-full max-w-2xl aspect-video sm:max-w-lg lg:max-w-4xl bg-[#e0e5ec] rounded-[50px] shadow-neu gap-4 justify-center items-center p-4">
+          <div className="flex w-full bg-[#e0e5ec] rounded-[50px] shadow-neu h-1/2 max-h-[120px] lg:max-h-[550px]"></div>
+          <div className="flex flex-row w-full rounded-[50px] gap-2 lg:gap-4 h-1/2 justify-center items-center">
+            <div className="flex rounded-full bg-[#e0e5ec] w-1/2 max-w-[120px] lg:max-w-[230px] aspect-square border border-black"></div>
+            <div className="flex bg-[#e0e5ec] border border-black rounded-[50px] w-full h-full max-h-[120px] lg:max-h-[550px]"></div>
           </div>
         </div>
 
